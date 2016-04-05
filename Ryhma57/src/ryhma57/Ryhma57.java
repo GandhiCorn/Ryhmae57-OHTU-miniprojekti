c79ef681e012aa10fa27dbe017699138c7b3db63
jaahon <jan.a.ahonen@helsinki.fi>
2016-04-05 17:04:14 +0300

    Java project created

---
+++ Ryhma57/src/ryhma57/Ryhma57.java
@@ -0,0 +1,12 @@
+package ryhma57;
+
+public class Ryhma57 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}

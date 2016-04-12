66367e57ce318ecea80e254063baa4c0b5b54fec
jaahon <jan.a.ahonen@helsinki.fi>
2016-04-12 07:24:17 +0300

    Get and set field methods added for the book class

--- Ryhma57/src/ryhma57/backend/Book.java
+++ Ryhma57/src/ryhma57/backend/Book.java
@@ -1,5 +1,7 @@
 package ryhma57.backend;
 
+import static ryhma57.backend.BibtexReferenceField.*;
+
 public class Book implements Reference {
     private String id;
     private String author;
@@ -16,7 +18,39 @@ public class Book implements Reference {
     }
     
     public String toBibTex() {
-        return "@book{" + id + ",\nauthor = {" + author + "},\ntitle = {" + title + "},\nyear = {" + year + "},\npublisher = {" + publisher + "},\n}";
+        return "@book{" + id + ",\n" + "author = {" + author + "},\n" + "title = {" + title + "},\n" + "year = {" + year + "},\n" + "publisher = {" + publisher + "},\n" + "}";
+    }
+    
+    public void setField(BibtexReferenceField field, String value) {
+        if (field == ID) {
+            this.id = value;
+        } else if (field == AUTHOR) {
+            this.author = value;
+        } else if (field == TITLE) {
+            this.title = value;
+        } else if (field == YEAR) {
+            this.year = value;
+        } else if (field == PUBLISHER) {
+            this.publisher = value;
+        } else {
+            System.out.println("Invalid field");
+        }
+    }
+    
+    public String getField(BibtexReferenceField field) {
+        if (field == ID) {
+            return this.id;
+        } else if (field == AUTHOR) {
+            return this.author;
+        } else if (field == TITLE) {
+            return this.title;
+        } else if (field == YEAR) {
+            return this.year;
+        } else if (field == PUBLISHER) {
+            return this.publisher;
+        } else {
+            return "Invalid field";
+        }
     }
     
 }

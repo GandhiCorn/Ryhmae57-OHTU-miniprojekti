fa4b73afb030988b614b4169c1671f2a0088ec2d
Aleksi Salmela <aleksi.salmela@helsinki.fi>
2016-04-13 20:25:35 +0300

    Use abstract class for the book reference.

--- Ryhma57/src/ryhma57/backend/Book.java
+++ Ryhma57/src/ryhma57/backend/Book.java
@@ -1,56 +1,37 @@
 package ryhma57.backend;
 
+import java.util.EnumSet;
 import static ryhma57.backend.BibtexReferenceField.*;
 
-public class Book implements Reference {
-    private String id;
-    private String author;
-    private String title;
-    private String year;
-    private String publisher;
-    
+public class Book extends Reference {
+    static EnumSet<BibtexReferenceField> existingFields;
+    static EnumSet<BibtexReferenceField> requiredFields;
+
+    static {
+        Book.requiredFields = EnumSet.noneOf(BibtexReferenceField.class);
+        Book.requiredFields.add(ID);
+        Book.requiredFields.add(AUTHOR);
+        Book.requiredFields.add(TITLE);
+        Book.requiredFields.add(YEAR);
+        Book.requiredFields.add(PUBLISHER);
+        Book.existingFields = Book.requiredFields.clone();
+        
+    }
+
     public Book(String id, String author, String title, String year, String publisher) {
-        this.id = id;
-        this.author = author;
-        this.title = title;
-        this.year = year;
-        this.publisher = publisher;
+        super(existingFields, requiredFields);
+        setField(ID, id);
+        setField(AUTHOR, author);
+        setField(TITLE, title);
+        setField(YEAR, year);
+        setField(PUBLISHER, publisher);
     }
     
     public String toBibTex() {
-        return "@book{" + id + ",\n" + "author = {" + author + "},\n" + "title = {" + title + "},\n" + "year = {" + year + "},\n" + "publisher = {" + publisher + "},\n" + "}";
+        return "@book{" + getField(ID) + ",\n" +
+                "author = {" + getField(ID) + "},\n" +
+                "title = {" + getField(TITLE) + "},\n" +
+                "year = {" + getField(YEAR) + "},\n" +
+                "publisher = {" + getField(PUBLISHER) + "},\n" + "}";
     }
-    
-    public void setField(BibtexReferenceField field, String value) {
-        if (field == ID) {
-            this.id = value;
-        } else if (field == AUTHOR) {
-            this.author = value;
-        } else if (field == TITLE) {
-            this.title = value;
-        } else if (field == YEAR) {
-            this.year = value;
-        } else if (field == PUBLISHER) {
-            this.publisher = value;
-        } else {
-            System.out.println("Invalid field");
-        }
-    }
-    
-    public String getField(BibtexReferenceField field) {
-        if (field == ID) {
-            return this.id;
-        } else if (field == AUTHOR) {
-            return this.author;
-        } else if (field == TITLE) {
-            return this.title;
-        } else if (field == YEAR) {
-            return this.year;
-        } else if (field == PUBLISHER) {
-            return this.publisher;
-        } else {
-            return "Invalid field";
-        }
-    }
-    
 }
--- Ryhma57/src/ryhma57/backend/Reference.java
+++ Ryhma57/src/ryhma57/backend/Reference.java
@@ -1,7 +1,32 @@
 package ryhma57.backend;
 
-public interface Reference {
-    
-    String toBibTex();
-    
+import java.util.EnumMap;
+import java.util.EnumSet;
+
+public abstract class Reference {
+    protected final EnumSet<BibtexReferenceField> existingFields;
+    protected final EnumSet<BibtexReferenceField> requiredFields;
+    private EnumMap<BibtexReferenceField, String> fields;
+
+    protected Reference(EnumSet<BibtexReferenceField> existingFields,
+            EnumSet<BibtexReferenceField> requiredFields) {
+        this.fields = new EnumMap<>(BibtexReferenceField.class);
+        this.existingFields = existingFields;
+        this.requiredFields = requiredFields;
+    }
+
+    public final String getField(BibtexReferenceField field) {
+        return fields.get(field);
+    }
+
+    public final void setField(BibtexReferenceField field, String value) {
+        if(!existingFields.contains(field)) {
+            //FIXME we should propably throw exception here.
+            System.out.println("Invalid field");
+            return;
+        }
+        fields.put(field, value);
+    }
+
+    abstract String toBibTex();
 }

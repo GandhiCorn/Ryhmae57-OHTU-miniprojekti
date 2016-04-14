1fc6ca5fc506c57206d961bc81b7aad144e3dda5
Aleksi Salmela <aleksi.salmela@helsinki.fi>
2016-04-14 22:51:07 +0300

    Add the editor field to the required fields at the book ref.

--- Ryhma57/src/ryhma57/references/Book.java
+++ Ryhma57/src/ryhma57/references/Book.java
@@ -10,7 +10,7 @@ public class Book extends Reference {
 
     static {
         Book.requiredFields = Reference.createFieldSet(AUTHOR,
-                TITLE, YEAR, PUBLISHER);
+                EDITOR, TITLE, YEAR, PUBLISHER);
         Book.optionalFields = Reference.createFieldSet(VOLUME,
                 NUMBER, SERIES, ADDRESS, EDITION, MONTH, NOTE);
         Book.existingFields = Reference.createExistingSet(

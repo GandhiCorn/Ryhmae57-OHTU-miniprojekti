00a543e7b28f72400dedaee1f2ae9317e4a5e1b4
Tuisku Haro <tuisku.haro@gmail.com>
2016-05-03 10:34:57 +0300

    add tag to reference test

---
+++ Ryhma57/src/test/easyb/add_tag_to_reference_Story.groovy
@@ -0,0 +1,30 @@
+import ryhma57.backend.*
+import ryhma57.gui.*
+import ryhma57.references.*
+import static ryhma57.backend.ReferenceType.*
+import java.nio.file.Files
+import java.nio.file.Paths
+
+scenario "Lisää uusi kirja referenssi varustettuna tagillä",{
+    given "Applikaatio on olemassa ja meillä on fieldit", {
+        Storage.removeTmpFiles()
+        app = new Application()
+        app.run(null)
+        fields = new EnumMap<BibtexReferenceField, String>(BibtexReferenceField.class)
+        fields.put(BibtexReferenceField.ID, "test")
+	fields.put(BibtexReferenceField.AUTHOR, "Matti Nykänen")
+	fields.put(BibtexReferenceField.TITLE, "Kotkan lento")
+	fields.put(BibtexReferenceField.YEAR, "2002")
+	fields.put(BibtexReferenceField.PUBLISHER, "Otava")
+        fields.put(BibtexReferenceField.TAGS, "tägi")
+    }
+    when "Luo referenssi", {
+        app.createNewReference(BOOK, fields)
+    }
+    then "Tarkista että referenssi ollaan luotu", {
+        app.getList().size().shouldBe 1
+        (app.getList().get(0) instanceof Book).shouldBe true
+        app.getList().get(0).getField(BibtexReferenceField.TAGS).shouldBe "tägi"
+
+    }
+}
